8b1ddd12345944cfac8dda269bc73bf863b757cf
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-03-25 17:57:08 +0530

    infra

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "81s-remote-state-dev"
-    key    = "expense-vpc2"
+    key    = "expense-vpc"
     region = "us-east-1"
     dynamodb_table = "81s-locking-dev"
   }
